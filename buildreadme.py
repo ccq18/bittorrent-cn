5d917bdcd3f52f3f81826437fe30436251086665
ccq18 <348578429@qq.com>
2020-06-04 14:50:34 +0800

    no message

--- buildreadme.py
+++ buildreadme.py
@@ -6,7 +6,7 @@ import os
 g = os.walk(r"./")
 
 flist = []
-content = '# BitTorrent协议中文翻译 \n \n'
+content = '# BitTorrent协议中文翻译（p2p协议,p2p网络，对等网络）  \n \n'
 for path, dir_list, file_list in g:
     for file_name in file_list:
         if file_name.endswith('.md'):
@@ -15,12 +15,7 @@ for path, dir_list, file_list in g:
 flist.sort()
 for file_name in flist:
     name = file_name[:-3].strip()
-    content += '[%s](%s)\n' % (name, file_name)
-
-
-
-
-
+    content += '[%s](https://github.com/ccq18/bittorrent-cn/%s)  \n' % (name, file_name)
 
 
 f = open('./readme.md', 'w')
